13652f2cb62ebf48c67fb9545b49e06638d71ded
JonHimself <jcbrstudio@gmail.com>
2021-07-26 16:18:59 -0500

    Further Study Addtition of Exponent Operations

--- calc.js
+++ calc.js
@@ -1,21 +1,51 @@
 const readline = require('readline');
 
 const reader = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
+	input: process.stdin,
+	output: process.stdout,
 });
 
 
 
-reader.question("What would you like to calculate?", function(input){
+reader.question("What would you like to calculate?", function (input) {
 	tokens = input.split(' ');
-	
+
 	mathSymbol = tokens[0];
 	num1 = Number(tokens[1]);
 	num2 = Number(tokens[2]);
 
+	// console.log('mathSymbol', mathSymbol, 'num1', num1, 'num2', num2);
 
+	if (mathSymbol === "+") {
+		console.log(num1 + num2);
+	}
+
+	if (mathSymbol === "-") {
+		console.log(num1 - num2);
+	}
+
+	if (mathSymbol === "*") {
+		console.log(num1 * num2);
+	}
+
+	if (mathSymbol === "/") {
+		console.log(num1 / num2);
+	}
+
+	if (mathSymbol === "squareRoot") {
+		console.log(Math.sqrt(num1));
+	}
+
+	if (mathSymbol === "^") {
+		console.log(Math.pow(num1, num2));
+	}
+
+	if (mathSymbol === "%") {
+		console.log(num1 % num2);
+	}
+	
 	// This line closes the connection to the command line interface.
 	reader.close()
-
 });
+
+
